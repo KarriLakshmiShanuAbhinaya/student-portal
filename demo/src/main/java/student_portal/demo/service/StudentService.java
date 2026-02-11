bc4a5265f4a4f59893f2f5b9b287896e17561919
KarriLakshmiShanuAbhinaya <abhinayakarri2003@gmail.com>
2026-02-11 20:41:33 +0530

    initial commit

--- demo/src/main/java/student_portal/demo/service/StudentService.java
+++ demo/src/main/java/student_portal/demo/service/StudentService.java
@@ -16,8 +16,7 @@ public class StudentService {
     @Autowired
     private StudentRepository studentRepo;
 
-    @Autowired
-    private JavaMailSender mailSender;
+
 
     // ================== STUDENT REGISTRATION ==================
     // Generates branch-wise UserID and sends mail
@@ -31,41 +30,20 @@ public class StudentService {
 
         Student savedStudent = studentRepo.save(student);
 
-        sendUserIdMail(savedStudent.getEmail(), savedStudent.getUserId());
+//        sendUserIdMail(savedStudent.getEmail(), savedStudent.getUserId());
 
         return savedStudent;
     }
 
     // ================== EMAIL USER ID ==================
 
-    private void sendUserIdMail(String toEmail, String userId) {
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Student Portal - Your User ID");
-        message.setText("Welcome!\n\nYour User ID is: " + userId
-                + "\n\nUse this User ID to login.");
-
-        mailSender.send(message);
-    }
 
     // ================== OTP GENERATION ==================
 
-    public int generateOtp() {
-        Random random = new Random();
-        return random.nextInt(900000) + 100000; // 6-digit OTP
-    }
 
     // ================== SEND OTP ==================
 
-    public void sendOtp(String toEmail, int otp) {
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Student Portal - Password Reset OTP");
-        message.setText("Your OTP for password reset is: " + otp);
-
-        mailSender.send(message);
-    }
 }
 
